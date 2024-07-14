2e59405ec3ef8fbbe10cfaf0a8873b22dab6c1cd
Kassio Kennedy <santoskassio562@gmail.com>
2024-07-13 22:56:20 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -38,3 +38,39 @@ cs('nav ul li a').forEach(link => {
 });
 
 
+// Slide header
+
+// let btnLeft = c('.fa-arrow-left');
+// let btnRight = c('.fa-arrow-right');
+// let slideItem = c('.slide-item');
+
+// btnLeft.addEventListener('click', (e)=>{
+//     slideItem.style.marginLeft = '-600px'
+// })
+
+
+let courrentSlide=0;
+
+function showSlide(index){
+    const slides = cs('.slide-item');
+    const totalSlide = slides.length;
+
+    if(index >= totalSlide){
+        courrentSlide = 0;
+    }else if(index < 0){
+        courrentSlide = totalSlide - 1;
+    }else{
+        courrentSlide = index
+    }
+
+
+    const offset = -courrentSlide * 600;
+    c('.slides--width').style.transform = `translateX(${offset}%)`;
+}
+
+function moveSlide(direction){
+    showSlide(courrentSlide + direction)
+    alert('ok')
+}
+
+showSlide(courrentSlide);
